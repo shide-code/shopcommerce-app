81a87b416df401106f106a7fa855478529544fdf
deriifirgiawan <derifirgiawan025@gmail.com>
2024-02-03 00:25:49 +0700

    feat: add theme colors

---
+++ themes/colors.ts
@@ -0,0 +1,69 @@
+export const colorsTheme = {
+	"--accent": {
+		normal: "#3B82F6",
+		hover: "#60A5FA",
+		press: "#2563EB",
+		active: "#3B82F6",
+		border: "#DBEAFE",
+		surface: "#EFF6FF",
+	},
+
+	"--informative": {
+		surface: "#F0F9FF",
+		"surface-banner": "#0284C7",
+		"surface-icon-banner": "#0369A1",
+		text: "#0C4A6E",
+		icon: "#0EA5E9",
+		"icon-banner": "#F0F9FF",
+		border: "#E0F2FE",
+	},
+
+	"--positive": {
+		surface: "#ECFDF5",
+		"surface-fill": "#10B981",
+		text: "#064E3B",
+		"text-hint": "#047857",
+		border: "#D1FAE5",
+		"border-fill": "#10B981",
+		icon: "#059669",
+		"strong-indicator": "#059669",
+	},
+
+	"--negative": {
+		suraface: "#FEF2F2",
+		text: "#7F1D1D",
+		"text-error": "#DC2626",
+		icon: "#DC2626",
+		border: "#FEE2E2",
+		"border-field": "#F87171",
+		button: "#EF4444",
+	},
+
+	"--notice": {
+		surface: "#FFFBEB",
+		text: "#78350F",
+		icon: "#D97706",
+		border: "#FEF3C7",
+	},
+
+	"--surface": {
+		1: "#FFFFFF",
+		2: "#FAFBFC",
+		3: "#F5F7FA",
+		4: "#F0F3F7",
+		5: "#EBEFF5",
+	},
+
+	"--text": {
+		high: "#0F172A",
+		medium: "#64748B",
+		low: "#CBD5E1",
+		disable: "#E2E8F0",
+	},
+
+	"--border": {
+		high: "#E2E8F0",
+		medium: "#F1F5F9",
+		low: "#F8FAFC",
+	},
+};
